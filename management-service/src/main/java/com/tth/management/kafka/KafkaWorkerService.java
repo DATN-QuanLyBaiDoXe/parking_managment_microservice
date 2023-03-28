5cd62a4610106fd0a9daa6418da8bb0aa54d7b94
hangtt <hangtt@elcom.com.vn>
2023-03-28 17:02:50 +0700

    kafka python

--- management-service/src/main/java/com/tth/management/kafka/KafkaWorkerService.java
+++ management-service/src/main/java/com/tth/management/kafka/KafkaWorkerService.java
@@ -1,14 +1,19 @@
 package com.tth.management.kafka;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.JsonObject;
 import com.tth.management.model.Event;
 import com.tth.management.service.EventService;
+import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.Map;
 
 @Service
 public class KafkaWorkerService {
@@ -27,14 +32,22 @@ public class KafkaWorkerService {
     public void workerReceive(String json) {
         try {
             LOGGER.info(" [-->] Server received request for " + json);
-            ObjectMapper mapper = new ObjectMapper();
-            Event event = mapper.readValue(json, Event.class);
+            Event event = transformEvent(json);
             if(event != null) {
                 eventService.save(event);
             }
         } catch (Exception ex) {
             LOGGER.error("Error to handle ViolationEventDTO event >>> {}", ex.getMessage());
         }
+    }
+
+    private Event transformEvent(String json) {
+        JSONObject jsonObject = new JSONObject(json);
+        Event event = new Event();
+        event.setPlace(jsonObject.getString("place"));
+        event.setImage(jsonObject.getString("image"));
+        event.setCreatedDate(new Date());
+        return event;
     }
 
 }
